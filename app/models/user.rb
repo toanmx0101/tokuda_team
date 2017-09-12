db9034dd90d467ed1886ba585a49106d9dbc7b23
Hoang Tung Lam <hoanglam995@gmail.com>
2017-09-13 01:26:23 +0700

    Add validate model: user, product, order, order_detail, category, sale

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  VALID_PHONE_REGEX = \(?\d+\)?[-.\s]?\d+[-.\s]?\d+
+  VVALID_EMAIL_REGEX = /A[w+-.]+@[a-zd-.]+.[a-z]+z/i
+  has_many :order
+
+  validates_associated :orders
+
   validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 255},
-    format: {with: VALID_EMAIL_REGEX},
-    uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
-  validates :phone, presence: true, format: {with: VALID_PHONE_REGEX}
+  validates :email, presence: true, uniqueness: true, confirmation: true,
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}, confirmation: true,
+    allow_nil: true
+  validates :phone, presence: true, length: {minimum: 9, maximum: 15},
+    numericality: true
+  validates :sex, presence: true
+  validates :address, presence: true
+  validates :role, presence: true
+
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 end
