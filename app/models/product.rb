a4f445dca3f577c59a515dea8b16ef842ddfa265
Hoang Tung Lam <hoanglam995@gmail.com>
2017-09-14 15:53:56 +0700

    Fix association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_one :sale
-  has_many :order_detail
-  has_many :order, through: :order_detail
+  has_one :sale, dependent: destroy
+  has_many :order_details
+  has_many :orders, through: :order_detail
 end
