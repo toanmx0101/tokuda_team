19b92e83c953e66c6f483a59283bdc347f478c30
Hoang Tung Lam <hoanglam995@gmail.com>
2017-10-03 00:47:30 +0700

    sign in sign up

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'devise'
 gem 'carrierwave', '~> 1.0'
+gem "mini_magick", "3.8.0"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002120109) do
+ActiveRecord::Schema.define(version: 20171002160547) do
 
   create_table "categories", force: :cascade do |t|
     t.string "made_in"
@@ -59,14 +59,19 @@ ActiveRecord::Schema.define(version: 20171002120109) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "avatar"
     t.string "phone"
     t.string "address"
     t.boolean "sex"
-    t.boolean "role"
+    t.boolean "role", default: false
+    t.string "avatar"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
